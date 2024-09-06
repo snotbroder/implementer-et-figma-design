948792e25898030d05bf5f63cab4e73e8d922bd7
Joris <jote0002@stud.kea.dk>
2024-09-06 14:22:18 +0200

    sat scroll container ind. vrker ikke endnu

---
+++ src/js/card-content.js
@@ -0,0 +1,18 @@
+export const cardContent = [
+  {
+    title: "The Balance Sheet",
+    body: "The Balance Sheet is a summary of the assets and liabilities and equity of a business at a specific point of time.",
+  },
+  {
+    title: "Income Statement",
+    body: "The Income (Profit and Loss) Statement, commonly referred to as the P&L statement, summarizes the revenue and expenses for a specific time period (one month, one quarter, one year, etc.)",
+  },
+  {
+    title: "Financial Plan",
+    body: "Keep your information current and review the documents on a regular basis (monthly or more often if needed). Review them with key individuals within your company.",
+  },
+  {
+    title: "Financial Ratios",
+    body: "Ratios are useful when comparing your company with the competition on financial performance and also when benchmarking the performance of your company.",
+  },
+];
